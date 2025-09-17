cdba32310882ed06cfb5179076ed23604f3c5280
lax2024 <lakshminandao002@gmail.com>
2025-09-17 12:00:35 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home (){
+    return(
+        <div>
+            <h1>Book Finder</h1>
+            Search:<input type = "text"></input><button class="btn btn-primary" type="submit">Button</button>
+        </div>
+    )
+}
+
+export default Home;
